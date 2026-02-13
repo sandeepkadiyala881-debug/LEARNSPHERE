e7f73d3538c79d56bb397ba37ccd30035a2ad63f
sandeep kadiyala <sandeepkadiyala881@gmail.com>
2026-02-13 11:58:04 +0530

    Delete src directory

--- src/lib/PageNotFound.jsx
+++
@@ -1,8 +0,0 @@
-export default function PageNotFound() {
-  return (
-    <div style={{ padding: "40px" }}>
-      <h1>404 – Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
-    </div>
-  );
-}
